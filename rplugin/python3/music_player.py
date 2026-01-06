ac8fc9b3c6813415e373cad84b6fed078ee9f949
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2026-01-05 23:57:50 -0600

    refactor(python): added type hints, improved module calls, new checks
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- rplugin/python3/music_player.py
+++ rplugin/python3/music_player.py
@@ -1,41 +1,58 @@
+"""Python code for ``nvim-music-player``."""
+from os.path import exists, expanduser
+from shutil import which
+from subprocess import DEVNULL, Popen
+from typing import NoReturn, Tuple
+
 import pynvim
-import subprocess
-import os
 
 
 @pynvim.plugin
 class MusicPlayer:
-    def __init__(self, nvim):
-        self.nvim = nvim
-        self.process = None
+    """
+    Music player Neovim plugin class.
+
+    Parameters
+    ----------
+    nvim : pynvim.Nvim
+        The Neovim instance.
+    """
+
+    nvim: pynvim.Nvim
+    process: Popen | None
+
+    def __init__(self, nvim: pynvim.Nvim):
+        self.nvim: pynvim.Nvim = nvim
+        self.process: Popen | None = None
         self.nvim.out_write("🎵 nvim-music-player loaded\n")
 
-    @pynvim.command("MusicPlay", nargs=1)
-    def play(self, args):
-        path_from_lua = args[0]
-        actual_path = path_from_lua.replace('\\ ', ' ')
-        path = os.path.expanduser(actual_path)
+    @pynvim.command("MusicPlay", nargs=1, complete="file")
+    def play(self, args: Tuple[str]) -> NoReturn:
+        """Start playing the given music file."""
+        if not which("mpv"):
+            self.nvim.err_write("Unable to find mpv in PATH\n")
+            return
 
-        if not os.path.exists(path):
+        path = expanduser(args[0].replace('\\ ', ' '))
+        if not exists(path):
             self.nvim.err_write(f"❌ File not found: {path}\n")
             return
 
-        if self.process:
+        if self.process is not None:
             self.process.terminate()
 
-        self.process = subprocess.Popen(
-            ["mpv", "--no-video", path],
-            stdout=subprocess.DEVNULL,
-            stderr=subprocess.DEVNULL
-        )
-
+        self.process = Popen(["mpv", "--no-video", path], stdout=DEVNULL, stderr=DEVNULL)
         self.nvim.out_write(f"🎶 Playing: {path}\n")
 
     @pynvim.command("MusicStop", nargs=0)
-    def stop(self):
-        if self.process:
+    def stop(self) -> NoReturn:
+        """Stop the music player."""
+        if self.process is not None:
             self.process.terminate()
             self.process = None
             self.nvim.out_write("⏹ Music stopped\n")
+            return
+
+        self.nvim.out_write("Music already stopped\n")
 
 # vim: set ts=4 sts=4 sw=4 et ai si sta:
